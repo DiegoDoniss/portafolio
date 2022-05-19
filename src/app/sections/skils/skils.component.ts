da37c3559a60f6a6432f11ef484500c2a20a80dc
Diego Donis <diego.jdonis@gmail.com>
2022-05-19 09:05:37 -0600

    correccion de skills

--- src/app/sections/skils/skils.component.ts
+++ src/app/sections/skils/skils.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faCloud, faCoffee, faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faCloud, faDatabase } from '@fortawesome/free-solid-svg-icons';
 import { faAngular, faHtml5, faLinux, faPython, faRedhat, faWordpress } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
@@ -9,13 +9,13 @@ import { faAngular, faHtml5, faLinux, faPython, faRedhat, faWordpress } from "@f
 })
 export class SkilsComponent implements OnInit {
   skils = [
-    { icon: faHtml5, title: 'Front-end', content: ['html-5', 'css-3', 'javascript', 'typescript', 'sass', 'bootstrap'] },
-    { icon: faPython, title: 'Back-end', content: ['Nodejs', 'Php', 'C#', 'Python, Go'] },
+    { icon: faHtml5, title: 'Front-end', content: ['html-5', 'css-3', 'javascript', 'typescript', 'sass', 'bootstrap', 'tailwind'] },
+    { icon: faPython, title: 'Back-end', content: ['Javascript', 'Php', 'C#', 'Python', 'Go'] },
     { icon: faAngular, title: 'Frameworks', content: ['Angular', 'Flask', '.Net Core', 'React'] },
-    { icon: faDatabase, title: 'Bases de datos', content: ['SqlServer', 'MariaDB', 'MongoDB'] },
+    { icon: faDatabase, title: 'Bases de datos', content: ['SqlServer', 'MariaDB', 'Posgress', 'MongoDB'] },
     { icon: faLinux, title: 'Sistemas Operativos', content: ['Windows', 'Linux'] },
-    { icon: faCloud, title: 'Computacion en la nube', content: ['EC2', 'RDS', 'LAMDA', 'Server Less', 'Firebase', 'Azure'] },
-    { icon: faWordpress, title: 'Diseño web', content: ['Wordpress', 'Cpanel', 'Apache'] },
+    { icon: faCloud, title: 'Computacion en la nube', content: ['EC2', 'RDS', 'LAMDA', 'Server Less', 'Firebase', 'Azure', 'Digital Ocean', 'NGINX', 'Apache'] },
+    { icon: faWordpress, title: 'Diseño web', content: ['Wordpress', 'Cpanel'] },
   ];
   constructor() { }
 
